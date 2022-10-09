6b82e56454e1324ba50f0125089f0cb040554955
Igor <kugimn@list.ru>
2022-10-09 16:58:56 +0300

    50 +

---
+++ Task50/Program.cs
@@ -0,0 +1,81 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позицию элемента в двумерном массиве, и возвращает значение 
+// этого элемента или же указание, что такого элемента нет.
+// // Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+// 5 -> 9
+// индексы:
+// [0][1][2][3]
+// 1 4 7 2
+// [4][5][6][7]
+// 5 9 2 3
+// [8][9][10][11]
+// 8 4 2 4
+
+
+int[,] generate2DArray(int height, int width, int randomStart, int randomEnd)
+{
+    int[,] twoDArray = new int[height, width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            twoDArray[i, j] = new Random().Next(randomStart, randomEnd + 1);
+        }
+    }
+    return twoDArray;
+}
+
+
+void printColorData(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write(data);
+    Console.ResetColor();
+}
+void print2DArray(int[,] arrayToPrint)
+{
+    Console.Write(" \t");
+    for (int i = 0; i < arrayToPrint.GetLength(1); i++)
+    {
+        printColorData(i + "\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {
+        printColorData(i + "\t");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write(arrayToPrint[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void FindElement(int[,] array, int element)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (element == array[i, j])
+            {
+                Console.WriteLine($"Позиция элемента со значением {element} - [{i}, {j}]");
+                return;
+            }                  
+        }
+    }
+    Console.WriteLine("Такого элемента нет в массиве");
+}
+
+Console.Write("Введите число которое необходимо найти в массиве ");
+int ElementToFind = Convert.ToInt32(Console.ReadLine());
+
+int[,] generatedArray = generate2DArray(4, 6, 1, 20);
+print2DArray(generatedArray);
+Console.WriteLine();
+
+FindElement(generatedArray, ElementToFind);
